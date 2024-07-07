e2eded272f4e98ca8e275ef7d38299d316cf8039
Нерсан Назифов <N.Nazifov21g@pmgrz.net>
2024-07-08 00:00:11 +0300

    Add project files.

---
+++ zoologicheskata gradina na nersan i stefani/Animals.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zoologicheskata_gradina_na_nersan_i_stefani
+{
+    internal class Animals
+    {
+        public string AnimalId { get; set; }
+        public string Species { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Habitat { get; set; }
+        public bool Availability { get; set; }
+    }
+
+}
---
+++ zoologicheskata gradina na nersan i stefani/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zoologicheskata_gradina_na_nersan_i_stefani
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
