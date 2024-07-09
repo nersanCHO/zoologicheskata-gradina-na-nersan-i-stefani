fef9270bbd2da0e808395f3a980e82507642fd72
Стефани Радулкова <S.Radulkova21g@pmgrz.net>
2024-07-09 14:50:12 +0300

    metod

--- zoologicheskata gradina na nersan i stefani/Program.cs
+++ zoologicheskata gradina na nersan i stefani/Program.cs
@@ -11,5 +11,31 @@ namespace zoologicheskata_gradina_na_nersan_i_stefani
         static void Main(string[] args)
         {
         }
+        static List<Animals> LoadAnimalssFromFile(string path)
+        {
+            List<Animals> Animals = new List<Animals>();
+
+            if (File.Exists(path))
+            {
+                var lines = File.ReadAllLines(path);
+
+                foreach (var line in lines)
+                {
+                    var parts = line.Split(',');
+                    Animals.Add(new Animals
+                    {
+                        AnimalId = parts[0],
+                        Species = parts[1],
+                        Name = parts[2],
+                        Age = int.Parse(parts[3]),
+                        Habitat = parts[4],
+                        Availability = bool.Parse(parts[5])
+                    });
+                }
+            }
+
+            return Animals;
+        }
+
     }
 }
